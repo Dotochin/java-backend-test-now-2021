ce33045c39781f650bd6c89db42a1187c851a497
Dotochin <Dotochin21@gmail.com>
2021-11-10 17:52:39 +0700

    Automate testing with rest-assured

---
+++ src/test/java/com/geekbrains/HW_3/ImgurApiParamsHW.java
@@ -0,0 +1,9 @@
+package com.geekbrains.HW_3;
+
+public class ImgurApiParamsHW {
+
+    public static String TOKEN = "51bbaf22ca98b8c682408d85221debde8956924e";
+    public static String API_URL = "https://api.imgur.com";
+    public static String API_VERSION = "3";
+
+}
